42c79b1935832bd37a198ab5cf9f5f27399cdab4
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-01-29 12:35:13 +0200

    Add initial JavaScript files and update HTML structure

---
+++ app.js
@@ -0,0 +1,23 @@
+let age = 24;
+let userName = "Nathan";
+let hobby1 = ["reading", "coding", "gaming"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 100000,
+  company: "Google",
+};
+let adultYears;
+
+function calculateAdultYears(age) {
+  return age - 18;
+}
+
+let person = {
+  name: "Nathan", //Property
+  greet() {
+    alert("Hello, " + this.name);
+  },
+};
+person.greet();
+console.log("Nathan" - "athan");
